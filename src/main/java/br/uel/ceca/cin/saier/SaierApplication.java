8a7f06a1f39c69e4a904aa954017f26a8d8a9239
Renato Henrique <renato.henrique_98@hotmail.com>
2018-10-12 01:35:40 -0300

    criando paginas de erro 404 e 500

--- src/main/java/br/uel/ceca/cin/saier/SaierApplication.java
+++ src/main/java/br/uel/ceca/cin/saier/SaierApplication.java
@@ -4,11 +4,8 @@
 package br.uel.ceca.cin.saier;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class SaierApplication {
 
