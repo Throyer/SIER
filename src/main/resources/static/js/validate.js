0c54e0b098325740868fb0eb8d9ae8e989549b81
Renato Henrique <renato.henrique_98@hotmail.com>
2019-11-09 18:08:34 -0300

    Melhorias - separando a especificação da listagem - melhorias nas labels e organização

--- src/main/resources/static/js/validate.js
+++ src/main/resources/static/js/validate.js
@@ -1,16 +1,18 @@
 /**
- * @author Bootstrap 4 examples. https://getbootstrap.com/docs/4.1/examples/checkout/
+ * @author Bootstrap 4
+ * @description https://getbootstrap.com/docs/4.1/examples/checkout/
  */
 (function () {
     'use strict';
 
-    window.addEventListener('load', function () {
+    window.addEventListener('load', () => {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         let forms = document.getElementsByClassName('needs-validation');
 
         // Loop over them and prevent submission
-        let validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
+        let validation = Array.prototype.filter.call(forms, form => {
+
+            form.addEventListener('submit', event => {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
