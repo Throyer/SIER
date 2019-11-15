dc2957ab4808d31fd01cd92838d618532f4a63b3
Renato Henrique <renato.henrique_98@hotmail.com>
2019-11-15 18:16:14 -0300

    Util para os cargos

---
+++ src/main/java/com/github/websier/sier/app/utils/Cargos.java
@@ -0,0 +1,11 @@
+package com.github.websier.sier.app.utils;
+
+/**
+ * Cargos
+ */
+public class Cargos {
+
+    public static final String ADMINISTRADOR = "ADMINISTRADOR";
+    public static final String PROFESSOR = "PROFESSOR";
+    public static final String ALUNO = "ALUNO";
+}
